a667db4383e18fb82e32c46bd23cd5086078c4a9
murselnet <murselx@gmail.com>
2025-06-12 12:29:42 +0300

    lambda kodu ana dizine taşındı

---
+++ lambda_function.py
@@ -0,0 +1,23 @@
+
+"""
+import requests
+
+def lambda_handler(event, context):
+    response = requests.get("https://api.github.com")
+    return {
+        "statusCode": 200,
+        "body": response.json()
+    }
+"""
+
+
+
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda! From MRS MURSEL GitHub Actions!')
+    }
+
